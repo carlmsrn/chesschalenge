6416abd66a4c22107bbd5c89e2062b3d35ee1fec
carlmsrn <111371223+carlmsrn@users.noreply.github.com>
2023-12-01 14:30:33 -0300

    my_chess3.0.2

--- my_chess3.py
+++ my_chess3.py
@@ -1,5 +1,5 @@
-
 import chess
+import time
 
 tabuleiro = chess.Board()
 
@@ -63,7 +63,13 @@ kingstable = [
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30]
 
+tabuleiro_cache = {}
+
 def avaliar_tabuleiro():
+    board_key = tabuleiro.fen()
+    if board_key in tabuleiro_cache:
+        return tabuleiro_cache[board_key]
+
     if tabuleiro.is_checkmate():
         if tabuleiro.turn:
             return -9999
@@ -101,25 +107,44 @@ def avaliar_tabuleiro():
 
     avaliacao = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
 
-    if tabuleiro.turn:
-        return avaliacao
-    else:
-        return -avaliacao
+    tabuleiro_cache[board_key] = avaliacao
+    return avaliacao
+
+transposition_table = {}
 
 def alphabeta(alpha, beta, profundidade):
+    board_key = tabuleiro.fen()
+    if board_key in transposition_table:
+        entry = transposition_table[board_key]
+        if entry['depth'] >= profundidade:
+            if entry['flag'] == 'exact':
+                return entry['score']
+            elif entry['flag'] == 'lowerbound':
+                alpha = max(alpha, entry['score'])
+            elif entry['flag'] == 'upperbound':
+                beta = min(beta, entry['score'])
+            if alpha >= beta:
+                return entry['score']
+
     melhor_pontuacao = -9999
     if profundidade == 0:
         return quiesce(alpha, beta)
-    for movimento in tabuleiro.legal_moves:
+    for movimento in sorted(tabuleiro.legal_moves, key=heuristica_de_ordem):
         tabuleiro.push(movimento)
         pontuacao = -alphabeta(-beta, -alpha, profundidade - 1)
         tabuleiro.pop()
+
+        # Atualizar a tabela de transposição
         if pontuacao >= beta:
-            return pontuacao
+            transposition_table[board_key] = {'depth': profundidade, 'flag': 'lowerbound', 'score': beta}
+            return beta
         if pontuacao > melhor_pontuacao:
             melhor_pontuacao = pontuacao
         if pontuacao > alpha:
             alpha = pontuacao
+
+    # Atualizar a tabela de transposição
+    transposition_table[board_key] = {'depth': profundidade, 'flag': 'exact', 'score': melhor_pontuacao}
     return melhor_pontuacao
 
 def quiesce(alpha, beta):
@@ -139,12 +164,24 @@ def quiesce(alpha, beta):
                 alpha = pontuacao
     return alpha
 
-def escolher_movimento(profundidade, color):
+def heuristica_de_ordem(move):
+    if tabuleiro.is_capture(move):
+        return 1
+    elif not tabuleiro.gives_check(move) and chess.square_distance(move.from_square, move.to_square) <= 2:
+        return 0
+    else:
+        return -1
+
+
+def escolher_movimento(profundidade):
     melhor_movimento = chess.Move.null()
     melhor_pontuacao = -9999
     alpha = -10000
     beta = 10000
-    for movimento in tabuleiro.legal_moves:
+
+    start_time = time.time()  # Grava o tempo inicial
+
+    for movimento in sorted(tabuleiro.legal_moves, key=heuristica_de_ordem):
         tabuleiro.push(movimento)
         pontuacao_tabuleiro = -alphabeta(-beta, -alpha, profundidade - 1)
         if pontuacao_tabuleiro > melhor_pontuacao:
@@ -154,82 +191,80 @@ def escolher_movimento(profundidade, color):
             alpha = pontuacao_tabuleiro
         tabuleiro.pop()
 
-    #print(f"\n{tabuleiro} \n")
+    end_time = time.time()  # Grava o tempo final
+    elapsed_time = end_time - start_time
+
+    print(f"\nTempo de processamento: {elapsed_time:.3f} segundos")
+
+    return melhor_movimento
+
+# Pergunta ao jogador se ele quer ser as peças brancas ou pretas
+cor_peca = input("Escolha as peças que deseja jogar (Branco ou Preto): ").lower()
+
+def get_piece_moves(board, square):
+    piece = board.piece_at(square)
+    if piece is not None:
+        return [move.uci() for move in board.legal_moves if move.from_square == square]
+    return []
+
+# Loop principal do jogo
+while not tabuleiro.is_game_over():
+    print("----------------------------------")
+    print(f"\n{tabuleiro}\n")
+    while True:
+        escolher_profundidade = input("Defina a profundidade (1 a 4): ")
+
+        if escolher_profundidade.isdigit():  # Verifica se a entrada é um número
+            escolher_profundidade = int(escolher_profundidade)
+
+            if 1 <= escolher_profundidade <= 4:
+                break  # Sai do loop se o número estiver no intervalo desejado
+            else:
+                print("Por favor, insira um número entre 1 e 4.")
+        else:
+            print("Por favor, insira um número válido.")
+
+        # Agora você pode usar a variável escolher_profundidade no restante do seu código.
+        print(f"Profundidade escolhida: {escolher_profundidade}")
+    
 
     for square in chess.SQUARES:
-        moves = get_piece_moves(tabuleiro, square, color)
+        moves = get_piece_moves(tabuleiro, square)
         if moves:
             piece_type = tabuleiro.piece_at(square).piece_type
             print(f"{chess.piece_name(piece_type)}: {', '.join(moves)}")
 
-    print(f"\nJogada do Bot: {melhor_movimento.uci()}\n")
-    return melhor_movimento
-
-# Pergunta ao jogador se ele quer ser as peças brancas ou pretas -------------------------------------
-cor_peca = input("Escolha as peças que deseja jogar (Branco ou Preto): ").lower()
-
-def get_piece_moves(board, square, color):
-    piece = board.piece_at(square)
-    if piece is not None and piece.color == color:
-        return [move.uci() for move in board.legal_moves if move.from_square == square]
-    return []
-
-
-# Loop principal do jogo
-while not tabuleiro.is_game_over():
     
-    print("--------------------------------------------")
-    print(f"\n{tabuleiro} \n")
-    
-    print("Peças disponíveis:\n")
-    for square in chess.SQUARES:
-        moves = get_piece_moves(tabuleiro, square, chess.WHITE if tabuleiro.turn == chess.WHITE else chess.BLACK)
-        if moves:
-            piece_type = tabuleiro.piece_at(square).piece_type
-            print(f"{chess.piece_name(piece_type)} em {chess.square_name(square)}: {', '.join(moves)}")
 
     if tabuleiro.turn == chess.WHITE:
         if cor_peca == 'branco':
-            # Se o jogador escolheu as peças brancas, ele faz a jogada
             move_uci = input("\nFaça sua jogada (em formato UCI, ex: 'e2e4'): ")
-            while not (len(move_uci) == 4 and move_uci[0].isalpha() and move_uci[1].isdigit() and move_uci[2].isalpha() and move_uci[3].isdigit()):
-                print("Entrada inválida. Tente novamente.")
-                move_uci = input("Faça sua jogada (em formato UCI, ex: 'e2e4'): ")
-
             move = chess.Move.from_uci(move_uci)
             while move not in tabuleiro.legal_moves:
                 print("Movimento inválido. Tente novamente.")
                 move_uci = input("\nFaça sua jogada (em formato UCI, ex: 'e2e4'): ")
                 move = chess.Move.from_uci(move_uci)
-
             tabuleiro.push(move)
         else:
-            # Se o jogador escolheu as peças pretas, o computador faz a jogada
-            computer_move = escolher_movimento(3, chess.BLACK)
+            computer_move = escolher_movimento(escolher_profundidade)
             tabuleiro.push(computer_move)
+            print(f"\nJogada do bot: {computer_move.uci()}")  # Exibe a jogada do bot
     else:
         # Peças pretas (jogador ou computador, dependendo da escolha do jogador)
         if cor_peca == 'preto':
-            move_uci = input("Faça sua jogada (em formato UCI, ex: 'e7e5'): ")
-            while not (len(move_uci) == 4 and move_uci[0].isalpha() and move_uci[1].isdigit() and move_uci[2].isalpha() and move_uci[3].isdigit()):
-                print("Entrada inválida. Tente novamente.")
-                move_uci = input("\nFaça sua jogada (em formato UCI, ex: 'e7e5'): ")
-
+            
+            move_uci = input("\nFaça sua jogada (em formato UCI, ex: 'e7e5'): ")
             move = chess.Move.from_uci(move_uci)
             while move not in tabuleiro.legal_moves:
                 print("Movimento inválido. Tente novamente.")
                 move_uci = input("\nFaça sua jogada (em formato UCI, ex: 'e7e5'): ")
                 move = chess.Move.from_uci(move_uci)
-
             tabuleiro.push(move)
         else:
-            computer_move = escolher_movimento(3, chess.WHITE)
+            computer_move = escolher_movimento(escolher_profundidade)
             tabuleiro.push(computer_move)
+            print(f"\nJogada do bot: {computer_move.uci()}")  # Exibe a jogada do bot
 
 # Exibe o resultado do jogo
 print("Fim do jogo. Resultado:", tabuleiro.result())
-
-
-
-
 
